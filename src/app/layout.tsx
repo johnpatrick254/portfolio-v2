ad4be88da72ba67121b2b20fbb4212bff138996e
johnpatrick254 <jpattrick538@gmail.com>
2024-06-09 12:12:09 +0300

    feat: add vercel site monitoring tools

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Jost } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ThemeProvider } from "@/providers/theme.provider";
 import Header from "@/components/nav/navigation";
 import { ToastContainer } from "react-toastify";
@@ -25,6 +27,8 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange  enableSystem>
           <Header />
+          <SpeedInsights />
+          <Analytics />
           {children}
           <Footer />
           <ToastContainer />
